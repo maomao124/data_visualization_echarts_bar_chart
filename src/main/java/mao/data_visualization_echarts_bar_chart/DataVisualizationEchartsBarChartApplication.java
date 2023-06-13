1161e4fd67b4d9b6ddb60822b496c605c28fc274
mao <1296193245@qq.com>
2023-06-14 00:29:18 +0800

    data_visualization_echarts_bar_chart

---
+++ src/main/java/mao/data_visualization_echarts_bar_chart/DataVisualizationEchartsBarChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_bar_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationEchartsBarChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationEchartsBarChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_echarts_bar_chart/DataVisualizationEchartsBarChartApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_bar_chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationEchartsBarChartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
